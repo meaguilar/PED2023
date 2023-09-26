b86101f3a652e7b5e94d42f02743f4cc110a9ed4
meaguilar <meaguilar@uca.edu.sv>
2023-09-26 09:16:38 -0600

    :memo:

---
+++ ListasEnlazadas.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+
+using namespace std;
+
+struct Nodo
+{
+    int elemento;
+    struct Nodo *siguiente;
+};
+
+struct Nodo *lista = nullptr;
+
+// Declaración de funciones
+void InsertarInicio(int);
+void InsertarFinal(int);
+void InsertarIntermedio(int, int);
+void Imprimir();
+void EliminarInicio();
+void EliminarFinal();
+
+int main()
+{
+    InsertarInicio(10);
+    InsertarInicio(5);
+    InsertarInicio(40);
+    InsertarIntermedio(400, 2);
+    Imprimir();
+    return 0;
+}
+
+// Insertar nodo al inicio de la lista
+void InsertarInicio(int n)
+{
+    // Reserva de memoria
+    struct Nodo *nuevoNodo = new Nodo();
+    // Variable auxiliar almacena el nodo actual
+    struct Nodo *temporal = lista;
+    nuevoNodo->elemento = n;
+    nuevoNodo->siguiente = nullptr;
+    // Si la lista esta vacia se agrega el primer nodo
+    if (lista == nullptr)
+    {
+        lista = nuevoNodo;
+    }
+    else
+    {
+        // Agregar un nodo al inicio
+        nuevoNodo->siguiente = lista;
+        lista = nuevoNodo;
+    }
+}
+
+// Insertar nodo al final de la lista
+void InsertarFinal(int n)
+{
+    // Reserva de memoria
+    struct Nodo *nuevoNodo = new Nodo();
+    // Variable auxiliar almacena el nodo actual
+    struct Nodo *temporal = lista;
+    nuevoNodo->elemento = n;
+    nuevoNodo->siguiente = nullptr;
+    // Si la lista esta vacia se agrega el primer nodo
+    if (lista == nullptr)
+    {
+        lista = nuevoNodo;
+    }
+    else
+    {
+        // Agregar un nodo al final
+        while (temporal->siguiente != nullptr)
+        {
+            temporal = temporal->siguiente;
+        }
+        temporal->siguiente = nuevoNodo;
+    }
+}
+
+void InsertarIntermedio(int n, int posicion)
+{
+    // Reserva de memoria
+    struct Nodo *nuevoNodo = new Nodo();
+    // Variable auxiliar almacena el nodo actual
+    struct Nodo *temporal = lista;
+    nuevoNodo->elemento = n;
+    nuevoNodo->siguiente = nullptr;
+
+    posicion--;
+
+    while (posicion != 1)
+    {
+        temporal = temporal->siguiente;
+        posicion--;
+    }
+    nuevoNodo->siguiente = temporal->siguiente;
+    temporal->siguiente = nuevoNodo;
+}
+
+void EliminarInicio()
+{
+    struct Nodo *temporal = lista;
+    // Si en la lista se encuentran elementos
+    if (lista != nullptr)
+    {
+        // Borra el elemento
+        lista = temporal->siguiente;
+        // Libera la memoria
+        delete temporal;
+    }
+    else
+    {
+        cout << "Lista vacia";
+    }
+}
+
+void EliminarFinal()
+{
+    struct Nodo *temporal = lista;
+    struct Nodo *temporal2;
+
+    if (lista != nullptr)
+    {
+        // Si el nodo no es el ultimo
+        if (temporal->siguiente != nullptr)
+        {
+            // Se realiza la iteraciones hasta encontra el ultimo nodo que apunte a nullptr
+            while (temporal->siguiente != nullptr)
+            {
+                temporal2 = temporal;
+                temporal = temporal->siguiente;
+            }
+            // Almacena el penultimo nodo sera quien apunte nullptr
+            temporal2->siguiente = nullptr;
+            // Se libera la memoria del nodo ultimo
+            delete temporal;
+            temporal = nullptr;
+        }
+        else // Si unicamente se encontraba un nodo en la lista
+        {
+            delete lista;
+            lista = nullptr;
+        }
+    }
+    else
+    {
+        cout << "Lista vacia";
+    }
+}
+
+void Imprimir()
+{
+    struct Nodo *temporal = lista;
+    if (lista != nullptr)
+    {
+        while (temporal != nullptr)
+        {
+            cout << "Lista " << temporal->elemento << " Direccion " << temporal << " dir nodo siguiente " << temporal->siguiente << endl;
+            temporal = temporal->siguiente;
+        }
+    }
+    else
+    {
+        cout << "Lista vacia";
+    }
+}
